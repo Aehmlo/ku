7b0d2a1937f76e832c14c7164bf4c1a20574117d
Alex Hamilton <alex.hamilton@ou.edu>
2018-08-18 22:48:50 -0500

    Update formatting for new rustfmt.

--- src/dimensions.rs
+++ src/dimensions.rs
@@ -1,172 +1,170 @@
-#[cfg(
-    any(
-        all(
-            feature = "2D",
-            any(
-                feature = "3D",
-                feature = "4D",
-                feature = "5D",
-                feature = "6D",
-                feature = "7D",
-                feature = "8D",
-                feature = "9D",
-                feature = "10D",
-                feature = "11D",
-                feature = "12D"
-            )
-        ),
-        all(
+#[cfg(any(
+    all(
+        feature = "2D",
+        any(
             feature = "3D",
-            any(
-                feature = "2D",
-                feature = "4D",
-                feature = "5D",
-                feature = "6D",
-                feature = "7D",
-                feature = "8D",
-                feature = "9D",
-                feature = "10D",
-                feature = "11D",
-                feature = "12D"
-            )
-        ),
-        all(
             feature = "4D",
-            any(
-                feature = "2D",
-                feature = "3D",
-                feature = "5D",
-                feature = "6D",
-                feature = "7D",
-                feature = "8D",
-                feature = "9D",
-                feature = "10D",
-                feature = "11D",
-                feature = "12D"
-            )
-        ),
-        all(
             feature = "5D",
-            any(
-                feature = "2D",
-                feature = "3D",
-                feature = "4D",
-                feature = "6D",
-                feature = "7D",
-                feature = "8D",
-                feature = "9D",
-                feature = "10D",
-                feature = "11D",
-                feature = "12D"
-            )
-        ),
-        all(
             feature = "6D",
-            any(
-                feature = "2D",
-                feature = "3D",
-                feature = "4D",
-                feature = "5D",
-                feature = "7D",
-                feature = "8D",
-                feature = "9D",
-                feature = "10D",
-                feature = "11D",
-                feature = "12D"
-            )
-        ),
-        all(
             feature = "7D",
-            any(
-                feature = "2D",
-                feature = "3D",
-                feature = "4D",
-                feature = "5D",
-                feature = "6D",
-                feature = "8D",
-                feature = "9D",
-                feature = "10D",
-                feature = "11D",
-                feature = "12D"
-            )
-        ),
-        all(
             feature = "8D",
-            any(
-                feature = "2D",
-                feature = "3D",
-                feature = "4D",
-                feature = "5D",
-                feature = "6D",
-                feature = "7D",
-                feature = "9D",
-                feature = "10D",
-                feature = "11D",
-                feature = "12D"
-            )
-        ),
-        all(
             feature = "9D",
-            any(
-                feature = "2D",
-                feature = "3D",
-                feature = "4D",
-                feature = "5D",
-                feature = "6D",
-                feature = "7D",
-                feature = "8D",
-                feature = "10D",
-                feature = "11D",
-                feature = "12D"
-            )
-        ),
-        all(
             feature = "10D",
-            any(
-                feature = "2D",
-                feature = "3D",
-                feature = "4D",
-                feature = "5D",
-                feature = "6D",
-                feature = "7D",
-                feature = "8D",
-                feature = "9D",
-                feature = "11D",
-                feature = "12D"
-            )
-        ),
-        all(
             feature = "11D",
-            any(
-                feature = "2D",
-                feature = "3D",
-                feature = "4D",
-                feature = "5D",
-                feature = "6D",
-                feature = "7D",
-                feature = "8D",
-                feature = "9D",
-                feature = "10D",
-                feature = "12D"
-            )
-        ),
-        all(
-            feature = "12D",
-            any(
-                feature = "2D",
-                feature = "3D",
-                feature = "4D",
-                feature = "5D",
-                feature = "6D",
-                feature = "7D",
-                feature = "8D",
-                feature = "9D",
-                feature = "10D",
-                feature = "11D"
-            )
+            feature = "12D"
+        )
+    ),
+    all(
+        feature = "3D",
+        any(
+            feature = "2D",
+            feature = "4D",
+            feature = "5D",
+            feature = "6D",
+            feature = "7D",
+            feature = "8D",
+            feature = "9D",
+            feature = "10D",
+            feature = "11D",
+            feature = "12D"
+        )
+    ),
+    all(
+        feature = "4D",
+        any(
+            feature = "2D",
+            feature = "3D",
+            feature = "5D",
+            feature = "6D",
+            feature = "7D",
+            feature = "8D",
+            feature = "9D",
+            feature = "10D",
+            feature = "11D",
+            feature = "12D"
+        )
+    ),
+    all(
+        feature = "5D",
+        any(
+            feature = "2D",
+            feature = "3D",
+            feature = "4D",
+            feature = "6D",
+            feature = "7D",
+            feature = "8D",
+            feature = "9D",
+            feature = "10D",
+            feature = "11D",
+            feature = "12D"
+        )
+    ),
+    all(
+        feature = "6D",
+        any(
+            feature = "2D",
+            feature = "3D",
+            feature = "4D",
+            feature = "5D",
+            feature = "7D",
+            feature = "8D",
+            feature = "9D",
+            feature = "10D",
+            feature = "11D",
+            feature = "12D"
+        )
+    ),
+    all(
+        feature = "7D",
+        any(
+            feature = "2D",
+            feature = "3D",
+            feature = "4D",
+            feature = "5D",
+            feature = "6D",
+            feature = "8D",
+            feature = "9D",
+            feature = "10D",
+            feature = "11D",
+            feature = "12D"
+        )
+    ),
+    all(
+        feature = "8D",
+        any(
+            feature = "2D",
+            feature = "3D",
+            feature = "4D",
+            feature = "5D",
+            feature = "6D",
+            feature = "7D",
+            feature = "9D",
+            feature = "10D",
+            feature = "11D",
+            feature = "12D"
+        )
+    ),
+    all(
+        feature = "9D",
+        any(
+            feature = "2D",
+            feature = "3D",
+            feature = "4D",
+            feature = "5D",
+            feature = "6D",
+            feature = "7D",
+            feature = "8D",
+            feature = "10D",
+            feature = "11D",
+            feature = "12D"
+        )
+    ),
+    all(
+        feature = "10D",
+        any(
+            feature = "2D",
+            feature = "3D",
+            feature = "4D",
+            feature = "5D",
+            feature = "6D",
+            feature = "7D",
+            feature = "8D",
+            feature = "9D",
+            feature = "11D",
+            feature = "12D"
+        )
+    ),
+    all(
+        feature = "11D",
+        any(
+            feature = "2D",
+            feature = "3D",
+            feature = "4D",
+            feature = "5D",
+            feature = "6D",
+            feature = "7D",
+            feature = "8D",
+            feature = "9D",
+            feature = "10D",
+            feature = "12D"
+        )
+    ),
+    all(
+        feature = "12D",
+        any(
+            feature = "2D",
+            feature = "3D",
+            feature = "4D",
+            feature = "5D",
+            feature = "6D",
+            feature = "7D",
+            feature = "8D",
+            feature = "9D",
+            feature = "10D",
+            feature = "11D"
         )
     )
-)]
+))]
 compile_error!(
     "More than one number of dimensions was specified, but this crate currently only supports \
      compiling for a single dimensionality at a time. Please choose a singular dimensionality and \
